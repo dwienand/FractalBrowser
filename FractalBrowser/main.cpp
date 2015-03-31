fef31e31be4d95398206bb1ad6852ce8daf4a618
Dominik <dwienand@cmu.edu>
2015-03-30 18:34:31 -0700

    Initial Commit

---
+++ FractalBrowser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FractalBrowser
+//
+//  Created by Dominik on 3/30/15.
+//  Copyright (c) 2015 Team060. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
