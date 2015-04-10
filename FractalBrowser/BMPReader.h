2181cee69fce9fafc677ebd895760853c009ac79
Dominik <dwienand@cmu.edu>
2015-04-10 15:48:09 -0700

    modified .gitignore

---
+++ FractalBrowser/BMPReader.h
@@ -0,0 +1,14 @@
+//
+//  BMPReader.h
+//  FractalBrowser
+//
+//  Created by Dominik Wienand on 4/10/15.
+//  Copyright (c) 2015 Team060. All rights reserved.
+//
+
+#ifndef __FractalBrowser__BMPReader__
+#define __FractalBrowser__BMPReader__
+
+#include <stdio.h>
+
+#endif /* defined(__FractalBrowser__BMPReader__) */
